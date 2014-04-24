9121701e0bdff7abb0310d80e023f0e0646f6fe8
phette23 <phette23@gmail.com>
2014-04-24 18:12:49 -0400

    basic message-passing between contentscript.js & background.html working

--- chrome-extension/app/scripts/background.js
+++ chrome-extension/app/scripts/background.js
@@ -1,9 +1,25 @@
+/*
+ * background.js:
+ * listen for messages from contentscript.js
+ * messages contain wp object with info about article
+ * construct DPLA API queries using that info
+ * when we have some API results, sendResponse back to contentscript.js
+ */
+
 'use strict';
 
+// boilerplate from yeoman
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
+chrome.runtime.onMessage.addListener( function ( request, sender, sendResponse ) {
+    var id = sender.tab.id;
+
+    console.log( 'Message from a content script at', sender.tab.url, 'with id', id );
+    console.dir(sender);
+
+    if ( request.hello === 'hello' ) {
+      sendResponse( { farewell: 'goodbye' } );
+    }
 });
--- chrome-extension/app/scripts/contentscript.js
+++ chrome-extension/app/scripts/contentscript.js
@@ -1,1 +1,20 @@
+/* global jQuery */
+/* jQuery is available since loaded as another content script
+ * using v. 1.8.3 since that's what Wikipedia uses, helps with consistency
+ *
+ * contentscript.js:
+ * parse the article's HTML into wp object
+ * send {wp} to background.js
+ * on response from background.js, insert results into DOM
+ */
+
 'use strict';
+
+var $ = jQuery,
+    wp = {
+        title: $('#firstHeading').text()
+    };
+
+chrome.runtime.sendMessage( JSON.stringify(wp), function (response) {
+    console.log(response);
+});
