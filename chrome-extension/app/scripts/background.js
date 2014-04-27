4f65f97eb81e685af89a5c0f243faa3ad9327309
phette23 <phette23@gmail.com>
2014-04-27 19:36:08 -0400

    basic extension working: displays results for query of title
    doesn't use fallbacks yet
    also doesn't do opt-in display of results

--- chrome-extension/app/scripts/background.js
+++ chrome-extension/app/scripts/background.js
@@ -1,3 +1,4 @@
+/* global $ */
 /*
  * background.js:
  * listen for messages from contentscript.js
@@ -8,18 +9,91 @@
 
 'use strict';
 
-// boilerplate from yeoman
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
-});
+// construct a query URI
+var buildURI = function (query) {
+    var base = 'http://api.dp.la/v2/items',
+        key = 'e4c036f3302aad8d8c188683967b9619';
 
-chrome.runtime.onMessage.addListener( function ( request, sender, sendResponse ) {
-    var id = sender.tab.id;
-
-    console.log( 'Message from a content script at', sender.tab.url, 'with id', id );
-    console.dir(sender);
-
-    if ( request.hello === 'hello' ) {
-      sendResponse( { farewell: 'goodbye' } );
+    return base + '?api_key=' + key + '&q=' + encodeURIComponent(query);
+},
+// truncate string if too long & add …
+trunc = function (str, int) {
+    // default to 60 char cutoff
+    var cutoff = parseInt(int, 10) || 60,
+        // lots of Hathi Trust titles end in ' /'
+        newStr = str.replace(/(\s\/)$/, '');
+    if (newStr.length > cutoff) {
+        // trim trailing whitespace of substring
+        return newStr.substr(0, cutoff).replace(/\s$/,'') + '&hellip;';
+    } else {
+        return newStr;
     }
+},
+// given DPLA doc, see if its type array contains 'image'
+isItAnImage = function (resource) {
+    var types = resource.type;
+    // type could be array or string
+    if ($.isArray(types)) {
+        for (var type in types) {
+            if (types.hasOwnProperty(type) && type.toLowerCase() === 'image') {
+                return true;
+            }
+        }
+        return false;
+    } else if (types && types.toLowerCase() === 'image') {
+        return true;
+    } else {
+        return false;
+    }
+},
+buildSuggestions = function (dpla) {
+    var items = dpla.docs,
+        current = {},
+        suggestions = [];
+
+    $.each(items, function (ind, item){
+        var res = item.sourceResource;
+
+        current.title = $.isArray(res.title) ? res.title[0] : res.title;
+        current.title = trunc(current.title);
+        current.uri = item.isShownAt;
+        current.isImage = isItAnImage(res);
+
+        suggestions.push(current);
+        current = {};
+    });
+
+    return suggestions;
+},
+// send XHR to DPLA, pass results to callback
+getDPLAresults = function (wp, cb) {
+    var xhr = new XMLHttpRequest(),
+        // @TODO only using title, need to employ fallbacks if no results
+        url = buildURI(wp.title);
+
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var dpla = JSON.parse(xhr.responseText),
+                suggestions = buildSuggestions(dpla);
+
+            cb(suggestions);
+        }
+    };
+
+    xhr.send();
+};
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // contentscript sends wp object with info about article
+    var wp = request,
+        id = sender.tab.id;
+
+    console.log('Message from a content script at', sender.tab.url);
+
+    getDPLAresults(wp, function (suggestions) {
+        // sendResponse never seems to work
+        // but this does
+        chrome.tabs.sendMessage(id, suggestions);
+    });
 });
--- chrome-extension/app/scripts/contentscript.js
+++ chrome-extension/app/scripts/contentscript.js
@@ -11,10 +11,104 @@
 'use strict';
 
 var $ = jQuery,
-    wp = {
-        title: $('#firstHeading').text()
-    };
+wp = {
+    title: $('#firstHeading').text(),
+    categories: [],
+    otherTitles: [],
+    redirects: [],
+    // find the categories on the page
+    getCategories: function () {
+        $('#mw-normal-catlinks li').each(function (index, el){
+            // this == current DOM el, not wp
+            wp.categories.push($(el).text());
+        });
+    },
+    // find any '"Foo" redirects here.' alternate titles
+    getOtherTitles: function () {
+        $('.dablink').each(function (index, el){
+            var test = $(el).text().match('"(.*)" redirects here.');
+            if (test) {
+                // this == current DOM el, not wp
+                wp.otherTitles.push(test[1]);
+            }
+        });
+    }
+},
+// on off-chance title contains unescaped HTML,
+// replace any angle brackets < > with HTML entities
+rmAngles = function (str) {
+    return str.replace('<','&lt;').replace('>','&gt;');
+},
+// put constructed HTML on DOM
+// @TODO but only if top of the article is in view
+addToDOM = function (html) {
+    // #mw-content-text is main body of article
+    $('#mw-content-text').prepend(html);
+    $('#wikipedpla').show('slow');
+},
+// add HTML to page based on info in suggestions array
+displaySuggestions = function (s) {
+    // this is a terrible way to construct HTML
+    // TODO: use a legit templating library like Mustache
+    var html = '<div id="wikipedpla" class="dablink" style="display:none;"><a href="http://dp.la">DPLA</a> ',
+        last = false,
+        len = s.length;
 
-chrome.runtime.sendMessage( JSON.stringify(wp), function (response) {
-    console.log(response);
-});
+    if (len === 1) {
+        html += 'item of possible interest:';
+        html += ' <a href="' + rmAngles(s[0].uri) + '"';
+        if (s[0].isImage) {
+            html += ' class="dp-img"';
+        }
+        html += '>' + rmAngles(s[0].title) + '</a>.';
+    } else {
+        html += 'items of possible interest:';
+        $.each(s, function (index, item) {
+            if (index + 1 === len) {
+                last = true;
+            }
+            if (last) {
+                html += ' & ';
+            }
+            html += ' <a href="' + rmAngles(item.uri) + '"';
+            if (item.isImage) {
+                html += ' class="dp-img"';
+            }
+            html += '>' + rmAngles(item.title);
+            if (!last) {
+                html += '</a>,';
+            } else {
+                html += '</a>.';
+            }
+        });
+    }
+
+    html += '</div>';
+
+    addToDOM(html);
+},
+init = function() {
+    // only execute on the Main (Articles) namespace
+    // the first tab, text "Articles", has an id
+    // of form "cs-nstab-$NAMESPACE"
+    var tab = $('li[id^="ca-nstab-"]'),
+        id = tab.attr('id'),
+        onMainPg = (tab.text() === 'Main Page');
+
+    if (id.substr(-4) === 'main' && !onMainPg) {
+        wp.getCategories();
+        wp.getOtherTitles();
+
+        chrome.runtime.sendMessage(wp, function (response) {
+            // this never fires but onMessage.addListener below does
+            console.log('got a response:', response);
+        });
+
+        chrome.runtime.onMessage.addListener(function (req) {
+            console.log('content script got msg');
+            displaySuggestions(req);
+        });
+    }
+};
+
+init();
